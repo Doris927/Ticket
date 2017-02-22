ccb427274413b1740b4710dc3a3a60319b176750
tammytangg <tammytangg@gmail.com>
2017-02-22 20:36:06 +0900

    ticket.schema

--- register.php
+++ register.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap -->
     <link href="plugin/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/reset.css" rel="stylesheet">
-    <link href="css/booking.css" rel="stylesheet">
+    <link href="css/register.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,7 +19,19 @@
     <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+
+<?php
+$logged_flag=false;
+session_start();
+//如果用户未登录，即未设置$_SESSION['user_id']时，执行以下代码
+if(isset($_SESSION['user_id'])){
+    $logged_flag=true;
+}
+?>
+
 <body>
+
+
 <nav class="navbar navbar-fixed-top navbar-inverse">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -36,20 +48,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
+                <li class="active"><a href="#">Event<span class="sr-only">(current)</span></a></li>
             </ul>
             <form class="navbar-form navbar-left">
                 <div class="form-group">
@@ -58,16 +57,25 @@
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
+                <li><a href="#" data-toggle="modal" data-target="#myModal">
+                        <?php
+                        if($logged_flag){
+                            echo 'My Page';
+                        }else{
+                            echo 'Log In';
+                        }
+                        ?>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <?php
+                        if($logged_flag){
+                            echo 'Log Out';
+                        }else{
+                            echo 'Sign Up';
+                        }
+                        ?>
+                    </a>
                 </li>
             </ul>
         </div><!-- /.navbar-collapse -->
@@ -75,8 +83,7 @@
 </nav>
 <div class="container">
     <div class="main-container">
-        <h1 class="title"><?php echo "我的第一段 PHP 脚本！";?>
-        </h1>
+        <h1 class="title">Sign Up</h1>
         <hr/>
         <div class="col-md-2"></div>
         <div class="col-md-8" style="padding-left: 0px;">
@@ -90,36 +97,37 @@
                         </div>
                         <div class="form-group">
                             <label for="inputPassword">Password:</label>
-                            <input type="password" class="form-control" id="inputPassword" placeholder="password">
+                            <input name="password" type="password" class="form-control" id="inputPassword" placeholder="password">
                         </div>
 
                         <div class="form-group">
                             <label for="inputFullName">Full Name:</label>
-                            <input type="number" class="form-control" id="inputFullName" placeholder="full name">
+                            <input  name="name" type="text" class="form-control" id="inputFullName" placeholder="full name">
                         </div>
 
                         <div class="form-group">
                             <label for="inputAddress">Address:</label>
-                            <input type="text" class="form-control" id="inputAddress" placeholder="address">
+                            <input name="address" type="text" class="form-control" id="inputAddress" placeholder="address">
                         </div>
                         <div class="form-group">
                             <label for="inputPhone">Phone:</label>
-                            <input type="number" class="form-control" id="inputPhone" placeholder="phone number">
+                            <input name="phone" type="text" class="form-control" id="inputPhone" placeholder="phone number">
                         </div>
                         <div class="form-group">
                             <label for="inputBirthday">Birthday:</label>
-                            <input type="number" class="form-control" id="inputBirthday" placeholder="YYYYMMDD">
+                            <input name="birthday" type="text" class="form-control" id="inputBirthday" placeholder="YYYYMMDD">
                         </div>
 
 
                         <label class="radio-inline">
-                            <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">Male
+                            <input type="radio" name="sex" id="inlineRadio1" value="0">Male
                         </label>
                         <label class="radio-inline">
-                            <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">Female
+                            <input type="radio" name="sex" id="inlineRadio2" value="1">Female
                         </label>
                         <br/><br/>
                         <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+                        <button id="btn-confirm" type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal-scan">Confirm</button>
                         <button type="button" class="btn btn-default btn-lg">Back</button>
                     </form>
                 </div>
@@ -149,13 +157,70 @@
     </div>
 </footer>
 
+<form id="getState" action="https://sapi.mysign.sdnp.ntt.ocn.ne.jp/pki/state" method="post">
+    <!--    <input type="hidden" name="scope" value="openid"/>-->
+    <!--    <input type="hidden" name="response_type" value="code"/>-->
+    <input type="hidden" name="client_id" value="JS3001"/>
+    <input type="hidden" name="service_id" value="S001"/>
+    <!--    <input type="hidden" name="redirect_uri" value="https://153.149.165.198/rp/register"/>-->
+    <!--    <input type="hidden" name="signature_data_type" value="text"/>-->
+    <!--    <input type="hidden" name="signature_data_source" value="param"/>-->
+    <!--    <input type="hidden" name="signature_data" value="test"/>-->
+
+</form>
+
+<!-- 模态框（Modal） -->
+<div class="modal fade" id="modal-scan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Please scan your my number card and enter the password</h4>
+            </div>
+            <div class="modal-body">
+                <div style="text-align: center;">
+                    <img src="img/cardscan.jpg"">
+                </div>
+                <form>
+                    <div class="form-group">
+                        <label for="inputPassword">Password:</label>
+                        <input type="password" class="form-control" id="inputPassword" placeholder="password">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button id="btn-modal-confirm" type="button" class="btn btn-primary btn-lg" data-dismiss="modal">Confirm</button>
+                <button type="button" class="btn btn-default btn-lg" data-dismiss="modal">Cancel</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal -->
+</div>
+
+<input type="hidden" id="hasConfirmed" value="0"/>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="plugin/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-<script>
 
+<script>
+    $(function() {
+        $("#btn-confirm").click(function () {
+//            $('#getState').submit();
+
+
+        });
+
+        $("#btn-modal-confirm").click(function () {
+            $('#inputFullName').val('田中　祐樹');
+            $('#inputAddress').val('滋賀県大津市松が丘6-1-1');
+            $('#inputBirthday').val('19930601');
+            $('#inputSex').val('123');
+            $('#inlineRadio1').attr("checked",true);
+
+        });
+
+    });
 </script>
 </body>
 </html>
